3d97d8f9a5b9c0be50e5500e705797fa1fd3497d
Ahmed <ahmedcisse1999@gmail.com>
2026-06-13 13:41:04 +0000

    Create superuser moussa

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('moussa', 'moussa@email.com', 'sebkha99')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.filter(username='moussa').exists() or User.objects.create_superuser('moussa', 'admin@email.com', 'sebkha99')" | python manage.py shell

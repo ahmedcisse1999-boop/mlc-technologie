3f0654b89bb245391bb3561265326259add66f42
Ahmed <ahmedcisse1999@gmail.com>
2026-06-13 13:07:56 +0000

    Create superuser

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@email.com', 'votre_mot_de_passe')" | python manage.py shell

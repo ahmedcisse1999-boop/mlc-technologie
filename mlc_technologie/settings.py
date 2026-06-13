aafc5dac8e10a5cfcebf4359cfbbc88ec60500da
Ahmed <ahmedcisse1999@gmail.com>
2026-06-13 12:14:43 +0000

    Fix STATIC_ROOT

--- mlc_technologie/settings.py
+++ mlc_technologie/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -114,6 +115,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LANGUAGE_CODE = 'fr-fr'
 TIME_ZONE = 'Africa/Nouakchott'
